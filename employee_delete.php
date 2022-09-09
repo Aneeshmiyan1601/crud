505fe2fa4107bc4d33dcdc1dd78487c80d70682e
Aneeshmiyan1601 <113176471+Aneeshmiyan1601@users.noreply.github.com>
2022-09-09 15:27:09 +0530

    Create employee_delete.php

---
+++ employee_delete.php
@@ -0,0 +1,26 @@
+<?php 
+
+// include "db.php"; 
+
+// if (isset($_GET['id'])) {
+
+//     $user_id = $_GET['id'];
+
+//     $sql = "DELETE FROM `employee` WHERE `id`='$user_id'";
+
+//      $result = $conn->query($sql);
+
+//      if ($result == TRUE) {
+        
+//         header('Location: index.php');
+
+//     }else{
+
+//         echo "Error:" . $sql . "<br>" . $conn->error;
+
+//     }
+
+// } 
+
+?>
+
