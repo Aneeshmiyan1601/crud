ce3e4d7b5659d65bfc3be6776c853c31c8dd08ee
Aneeshmiyan1601 <113176471+Aneeshmiyan1601@users.noreply.github.com>
2022-09-12 12:18:23 +0530

    Update employee_delete.php

--- employee_delete.php
+++ employee_delete.php
@@ -1,26 +1,34 @@
 <?php 
 
-// include "db.php"; 
+include "db.php"; 
 
-// if (isset($_GET['id'])) {
+if (isset($_GET['id'])) {
 
-//     $user_id = $_GET['id'];
+    $user_id = $_GET['id'];
+    $user_ids = $_GET['id'];
 
-//     $sql = "DELETE FROM `employee` WHERE `id`='$user_id'";
+    $sql = "DELETE FROM `employee` WHERE `id`='$user_id'";
+    $sql1 = "DELETE FROM `employee_demo` WHERE `id`='$user_ids'";
 
-//      $result = $conn->query($sql);
+     $result = $conn->query($sql);
+     $result1 = $conn->query($sql1);
 
-//      if ($result == TRUE) {
+     if ($result == TRUE) {
         
-//         header('Location: index.php');
+        // header('Location: admin.php');
 
-//     }else{
+    }if ($resul1 == TRUE) {
+        // header('Location: admin.php');
+        
+    }else{
 
-//         echo "Error:" . $sql . "<br>" . $conn->error;
+        // header('Location: admin.php');
+         echo '<script>alert("Permanently Record Delete Successfully")</script>';
+         echo '<script>window.location.href = "admin.php"; </script>';
 
-//     }
+    }
 
-// } 
+} 
 
 ?>
 
