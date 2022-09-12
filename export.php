880c5bba519e23916343b56fb2432bfb5d82a824
Aneeshmiyan1601 <113176471+Aneeshmiyan1601@users.noreply.github.com>
2022-09-12 12:20:35 +0530

    Create export.php

---
+++ export.php
@@ -0,0 +1,47 @@
+<?php
+	header("Content-Type: application/xls");    
+	header("Content-Disposition: attachment; filename=Employee_list.xls");  
+	header("Pragma: no-cache"); 
+	header("Expires: 0");
+ 
+	require_once 'db.php';
+ 
+	$output = "";
+ 
+	$output .="
+		<table>
+			<thead>
+				<tr>
+					<th>SI.NO</th>
+					<th>Employee Name</th>
+					<th>Employee Email</th>
+					<th>Employee Number</th>
+					<th>Employee ID</th>
+				</tr>
+			<tbody>
+	";
+ 
+	$query = $conn->query("SELECT * FROM `employee_demo`");
+	while($fetch = $query->fetch_array()){
+ 
+	$output .= "
+				<tr>
+					<td>".$fetch['id']."</td>
+					<td>".$fetch['employee_name']."</td>
+					<td>".$fetch['employee_email']."</td>
+					<td>".$fetch['employee_number']."</td>
+					<td>".$fetch['employee_id']."</td>
+				</tr>
+	";
+	}
+ 
+	$output .="
+			</tbody>
+ 
+		</table>
+	";
+ 
+	echo $output;
+ 
+ 
+?>
